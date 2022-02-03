7c4c96f37189361bffa20d6fa6651a3a445129f1
Rearles <53582049+Rearles@users.noreply.github.com>
2022-02-03 10:40:28 -0700

    Testing file

--- ArrayTest.cs
+++ ArrayTest.cs
@@ -4,19 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
+using PO_String_Array;
 
 
-namespace PO_String_Array
-{
-    public class ArrayTest
-    {
-        
-        FilterArray test = new FilterArray();
-        ArrayList array = [1, 2, "a", "b"];
-        ArrayList array2 = [1, "a", "b", 0, 15];
-        ArrayList array3 = [1, 2, "aasf", "1", "123", 123]
-        answer = new ArrayList();
-        answer = test.Filter(array);
+FilterArray filterarray = new FilterArray();
+ArrayList array1 = new ArrayList(){1, 2, "a", "b"};
+ArrayList array2 = new ArrayList(){1, "a", "b", 0, 15};
+ArrayList array3 = new ArrayList(){1, 2, "aasf", "1", "123", 123};
+ArrayList answer = new ArrayList();
 
-    }
-}
+answer = filterarray.Filter(array1);
+filterarray.Print(array1, answer, "Test 1:");
+
+answer = filterarray.Filter(array2);
+filterarray.Print(array2, answer, "Test 2:");
+
+answer = filterarray.Filter(array3);
+filterarray.Print(array3, answer, "Test 3:");
